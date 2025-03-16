6a5a674f98b59d9e0e559db64ece4f137af39bf1
PonMorin <pon.m22@gmail.com>
2025-03-16 15:09:54 +0700

    :hammer: Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ from dotenv import dotenv_values
 from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings import OpenAIEmbeddings
 from ContextualClass.contextual import ContextualRetrieval
+from intent.prompt_template import RAG_PROMPT_TEMPLATES, DYNAMIC_PROMPT_TEMPLATE
+from intent.intent import (
+    initialize_model,
+    predict_intent
+)
 
 # Load API key from environment file
 config = dotenv_values(".env")
@@ -16,37 +21,66 @@ context_data = "./doc_Data/context"
 
 
 model = ContextualRetrieval()
+tokenizer, intent_model = initialize_model()
 
-def chat_with_model(prompt, history, selected_option):
-    # Load vectorstores
-    contextual_vector = Chroma(persist_directory=context_data, embedding_function=OpenAIEmbeddings(), collection_name=f"cdti_doc")
+# def chat_with_model(prompt, history, selected_option):
+#     # Load vectorstores
+#     contextual_vector = Chroma(persist_directory=context_data, embedding_function=OpenAIEmbeddings(), collection_name=f"cdti_doc")
 
-    contextual_vector_results = contextual_vector.similarity_search(prompt, k=6)
-    contextual_vector_answer = model.generate_answer_api(prompt, [doc.page_content for doc in contextual_vector_results])
+#     contextual_vector_results = contextual_vector.similarity_search(prompt, k=6)
+#     contextual_vector_answer = model.generate_answer_api(prompt, [doc.page_content for doc in contextual_vector_results])
 
-    # Include the selected dropdown option in the response
-    response = f"[{selected_option}] {contextual_vector_answer}"
-    history.append((prompt, response))
-    return history, ""
+#     # Include the selected dropdown option in the response
+#     response = f"[{selected_option}] {contextual_vector_answer}"
+#     history.append((prompt, response))
+#     return history, ""
 
-def chat_with_model_history(prompt, history, selected_option):
+def intention(prompt: str):
+    try:        
+        # Initialize the model when the application starts. Load the model and tokenizer
+        # tokenizer, intent_model = initialize_model()
+
+        intent = predict_intent(prompt, tokenizer, intent_model)
+        
+        return intent
+    except Exception as e:
+        print(e)
+        return e
+
+def chat_with_model_history(prompt, history):
     dynamic_doc = ["academic_calendar", "student_activities"]
 
-    if selected_option in dynamic_doc:
-        with open("/home/s6410301020/SeniorProject/FDT_cdti_Chat/digital_doc/baseline.md", "r") as file:
-            contexts = file.read()
-        print(contexts)
-        answer, _ = model.generate_answer_api_dynamic_with_history(prompt, contexts)
-        response = f"[{selected_option}] {answer.content}]"
+    # intent = intention(prompt=prompt)
+    # print(intent)
+    # if intent in dynamic_doc:
+    #     if intent == "academic_calendar":
+    #         with open("D:\MyProject\FDT_cdti_Chat\doc\doc_test.md", "r") as file:
+    #             contexts = file.read()
+    #         print(contexts)
 
-    else:
-        contextual_vector = Chroma(persist_directory=context_data, embedding_function=OpenAIEmbeddings(), collection_name=f"cdti_doc")
-        retriever = contextual_vector.as_retriever(search_kwargs={"k": 6})
+    #         system_prompt = DYNAMIC_PROMPT_TEMPLATE(intent=intent, context=contexts)
+    #         answer, _ = model.generate_answer_api_dynamic_with_history(prompt, system_prompt=system_prompt)
+    #         response = f"[{intent}] {answer.content}]"
+    #     else:
+    #         with open("D:\MyProject\FDT_cdti_Chat\doc\doc_test.md", "r") as file:
+    #             contexts = file.read()
+    #         print(contexts)
 
-        answer, _ = model.generate_answer_api_with_history(prompt, retriever=retriever)
+    #         system_prompt = DYNAMIC_PROMPT_TEMPLATE(intent=intent, context=contexts)
+    #         answer, _ = model.generate_answer_api_dynamic_with_history(prompt, system_prompt=system_prompt)
+    #         response = f"[{intent}] {answer.content}]"
 
-        # Include the selected dropdown option in the response
-        response = f"[{selected_option}] {answer["answer"]}"
+    # else:
+    #     system_prompt = RAG_PROMPT_TEMPLATES[intent]
+    #     contextual_vector = Chroma(persist_directory=context_data, embedding_function=OpenAIEmbeddings(), collection_name=f"cdti_doc")
+    #     retriever = contextual_vector.as_retriever(search_kwargs={"k": 6})
+
+    #     answer, _ = model.generate_answer_api_with_history(prompt, retriever=retriever, system_prompt=system_prompt)
+
+    #     # Include the selected dropdown option in the response
+    #     response = f"[{intent}] {answer["answer"]}"
+
+    response = "Hello World"
 
 
     history.append((prompt, response))
@@ -92,8 +126,7 @@ with gr.Blocks(css=custom_css) as demo:
 
         chatbot_history = gr.Chatbot(
             label="Chat History",
-            elem_id="chatbox2",
-            bubble_full_width=False,
+            elem_id="chatbox2"
         )
 
     # state1 = gr.State([])
@@ -136,7 +169,7 @@ with gr.Blocks(css=custom_css) as demo:
 
     send_button2.click(
         fn=chat_with_model_history,
-        inputs=[input_box2, state2, dropdown2],
+        inputs=[input_box2, state2],
         outputs=[chatbot_history, input_box2]
     )
 
